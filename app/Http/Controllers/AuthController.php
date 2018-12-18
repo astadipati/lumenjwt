22d22eef467050fdce32c4a6dbcb9537bd165beb
rama astadipati <rama@ramas-MacBook-Pro.local>
2018-12-18 09:30:28 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+
+class AuthController extends BaseController
+{
+    public function register(Request $request)
+    {
+
+    }
+    public function login(Request $request)
+    {
+
+    }
+}
